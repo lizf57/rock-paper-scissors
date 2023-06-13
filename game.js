fc53b289107bdc3f9756394ac6e68f9b3782ff20
mona lisa <lizfriedman5@gmail.com>
2023-06-12 20:35:02 -0500

    add pseudocode

--- game.js
+++ game.js
@@ -1,1 +1,26 @@
-console.log("Hello RPS")
+// variables
+    // numWins
+    // numLosses
+    // numTies
+
+// Players choose R, P, S
+    // human choice - get choice from user input
+    // computer randomly chooses
+
+// compare choices
+    // if humanChoice is S and computerChoice is P OR
+    // if humanChoice is R and computerChoice is S OR
+    // if humanChoice is P and computerChoice is R 
+        // alert that we won
+        // increase wins
+    // if humanChoice === computerChoice 
+        // alert that we tied
+        // increase ties
+    // else    
+        // alert that we lost
+        // increase losses
+    // ask user "do you want to play again?"
+        // if yes
+            // restart game
+        // if no
+            // exit game
