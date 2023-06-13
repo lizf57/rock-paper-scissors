044a8db0532d96be46edaffaba13afbf8f22640b
mona lisa <lizfriedman5@gmail.com>
2023-06-12 21:33:03 -0500

    while loop

--- game.js
+++ game.js
@@ -4,38 +4,41 @@ var ties = 0
 
 var choices = ["R", "P" , "S"]
 
+var keepPlaying = true
+
+while (keepPlaying) {
+
 // human choice - get choice from user input
-var humanChoice = prompt("Choose R, P, or S")
-console.log(humanChoice)
+    var humanChoice = prompt("Choose R, P, or S")
 
-// computer randomly chooses
-var randomNum = Math.floor( Math.random()*3 )
-var computerChoice = choices[randomNum]
+    // computer randomly chooses
+    var randomNum = Math.floor( Math.random()*3 )
+    var computerChoice = choices[randomNum]
 
-console.log("Human: " + humanChoice)
-console.log("Computer: " + computerChoice)
+    console.log("Human: " + humanChoice)
+    console.log("Computer: " + computerChoice)
 
-// compare choices\
-if ( 
-    humanChoice === "S" && computerChoice === "P" ||
-    humanChoice === "R" && computerChoice === "S" ||
-    humanChoice === "P" && computerChoice === "R"  
-) { 
-    alert("You won!")    
-    wins++
-} else if (humanChoice === computerChoice) {
-    alert("Round tied!")
-    ties++
-} else {
-    alert("You lost!")
-    losses++
+    // compare choices\
+    if ( 
+        humanChoice === "S" && computerChoice === "P" ||
+        humanChoice === "R" && computerChoice === "S" ||
+        humanChoice === "P" && computerChoice === "R"  
+    ) { 
+        alert("You won!")    
+        wins++
+    } else if (humanChoice === computerChoice) {
+        alert("Round tied!")
+        ties++
+    } else {
+        alert("You lost!")
+        losses++
+    }
+
+    // show scores for user
+    alert("Wins: " + wins + "\nTies: " + ties + "\nLosses: " + losses)
+
+     keepPlaying = confirm("Do you want to play again?")  
+
 }
 
-// show scores for user
-alert("Wins: " + wins + "\nTies: " + ties + "\nLosses: " + losses)
-
-    // ask user "do you want to play again?"
-        // if yes
-            // restart game
-        // if no
-            // exit game
+console.log("Play again soon!")
