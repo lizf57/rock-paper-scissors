1e133ad973d2d0c45dbafcd479ea0c2639c2c5f1
mona lisa <lizfriedman5@gmail.com>
2023-06-12 20:59:59 -0500

    commit randomNum and humanChoice

--- game.js
+++ game.js
@@ -1,11 +1,16 @@
-// variables
-    // numWins
-    // numLosses
-    // numTies
+var wins = 0;
+var losses = 0
+var ties = 0
 
-// Players choose R, P, S
-    // human choice - get choice from user input
-    // computer randomly chooses
+var choices = ["R", "P" , "S"]
+
+// human choice - get choice from user input
+// var humanChoice = prompt("Choose R, P, or S")
+// console.log(humanChoice)
+
+// computer randomly chooses
+var randomNum = Math.floor( Math.random()*3 )
+console.log( choices[randomNum])
 
 // compare choices
     // if humanChoice is S and computerChoice is P OR
